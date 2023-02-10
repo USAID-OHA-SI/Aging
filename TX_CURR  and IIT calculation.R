906bcdda3224a813d9767dcb6d178f344f299a9b
Nelly Maina <87029048+nmaina@users.noreply.github.com>
2023-02-09 23:33:44 -0500

    FY22 LTFU

--- TX_CURR  and IIT calculation.R
+++ TX_CURR  and IIT calculation.R
@@ -4,26 +4,30 @@ library(dplyr)
 library(lubridate)
 library(purrr)
 
+
+end_fy <- as.Date("2022-09-30") 
 setwd("~/Data - 03FEB2023")
 
+
+# Appointments & Visits Data Import and Management ----------------------------------------------
 visits_data <- read_csv("Appointments.csv")
 
 #Renaming the variables and creating id2 based on id and facility
 visits_data <- visits_data %>% rename(im=`Implementing Partner`, facility=`Facility Name`, mfl_code=`Facility MFL Code`,
                                       id=`Client Number(De-identified)`, appoint_date=`Appointment Date`, 
                                       visit_date=`Actual Visit Date`) %>% 
-  mutate(Visit_id2 =  paste0(mfl_code, "_", id))
+  mutate(id2 =  paste0(mfl_code, "_", id))
 
-# change the dates  from character to date 
+# change the dates  from character to date format
 
-visits_data$visit_date <- as.Date(visits_data$visit_date, format = "%d/%m/%y")
-visits_data$appoint_date <- as.Date(visits_data$appoint_date, format = "%d/%m/%y")
+visits_data$visit_date <- as.Date(visits_data$visit_date, format = "%d/%m/%Y")
+visits_data$appoint_date <- as.Date(visits_data$appoint_date, format = "%d/%m/%Y")
 
 #Only working with clients who are in the master client list
 #Limitation: We'll need to drop duplicates based on id2
 # check for duplicates  based on id2 
 
-visits_data %>% distinct(Visit_id2) #88,100 distinct clients based on 1,800810 observations in the visits_dataset
+visits_data %>% distinct(id2) #88,100 distinct clients based on 1,800810 observations in the visits_dataset
 # extracting  Id2  from the visits and the master client list to match 
 
 # visits_id <- visits_data[, "Visit_id2"]
@@ -40,32 +44,42 @@ visits_data %>% distinct(Visit_id2) #88,100 distinct clients based on 1,800810 o
 # Id_merged <- merge(unique_visit_id, masterclient_id, by = c("id2"))
 
 #visits_data[["visit_id"]] <- row_number(visits_data)
+
+
+
+ # create masterlist_2 with  client and visit data 
 operator <- inner_join(visits_data, clientlist,by ="id2")
-n_distinct(visits_data$id2)-n_distinct(operator$id2)#0 
+n_distinct(visits_data$id2)-n_distinct(operator$id2)#0  veryfying id2 is a distint identifier
 # one more visits between July and September
 
 
 
-lost_null_visit <- filter(operator, map_lgl(visit_date, is.null))
+#lost_null_visit <- filter(operator, map_lgl(visit_date, is.null)) # filtering anything with a NUll visit date 
 
-operator2 <- filter(operator, !map_lgl(visit_date, is.null)) %>%
-  filter(month(visit_date) <= 9 & month(visit_date) >= 7) %>%
-  filter(date_art_init <= "2022-09-30") %>%
+end_fya <- end_fy - years(1) # calculating Yr ago 
+operator2 <- filter(operator, visit_date > !!end_fya & visit_date <= !!end_fy)  %>% # selecting only appointments in the last quarter of the FY
+  filter(date_art_init <= !!end_fy) %>%# Selecting only clients with initiation dates before 2022-09-30
+  filter(interval(dob, !!end_fy) / years(1) < 15) %>% 
   select(id2) %>%
-  inner_join(operator, by = "id2") %>%
+  inner_join(operator, by = "id2") %>% #  Merging back with the operator dataset to get their actual visits 
   select(id2,visit_date, reg_art_current) %>%
+  filter(visit_date <= !!end_fy) %>% 
   group_by(id2, reg_art_current) %>%
-  filter(visit_date == max(visit_date))
+  filter(visit_date == max(visit_date)) %>% 
+  distinct() %>% ## Steps A and B
+  ungroup()
 
-operator2$visit_date <- as.Date(operator2$visit_date, format = "%d/%m/%Y")
-operator2$trt_months <- 1
+operator2$trt_months <- 1 #  defaulting clients based on  regimen
 operator2[grepl("^\\(?3", operator2$reg_art_current), ]$trt_months <- 3
 operator2$next_expected_visit <- operator2$visit_date %m+% months(operator2$trt_months)
 operator2$lost_date <- operator2$next_expected_visit %m+% days(28)
 
 
+table(operator2$ trt_months)
+
 # operator 2 has ALL visits up till 2023
-end_fy <- as.Date("2022-09-30")
+# identifying clients  LFT  based on last visit  date  and 28 days after their last appointment date 
+ 
 
 # for trt with 3 month followup
 cutoff_1mo <- end_fy %m-% days(28) %m-% months(1)
@@ -79,7 +93,7 @@ lost_3mo_trt <- filter(operator2, trt_months == 3) %>%
   ungroup()
 
 # select the id2's of all the clients that have ven lost 
-lost_clients <- unique(rbind( select(lost_1mo_trt, id2), select(lost_3mo_trt, id2), select(lost_null_visit, id2)))
+lost_clients <- unique(rbind( select(lost_1mo_trt, id2), select(lost_3mo_trt, id2)))
 
 active_clients <- ungroup(operator2) %>%
   select(id2) %>%
@@ -101,9 +115,6 @@ lost_3mo_trt <- filter(operator2, trt_months == 3) %>%
 lost_clients <- rbind(lost_1mo_trt, lost_3mo_trt)
   
   
-  
-group_by(operator,id2,visit_date) %>% summarise(n=n()) %>% 
-  filter(n>1) %>% summarise(n=n())
 
 
 
